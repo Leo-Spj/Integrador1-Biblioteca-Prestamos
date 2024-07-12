a932778a7fb95c0d8f09d96f339c8516fd197be5
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-07-12 14:10:10 -0500

    metodo obtener historialUsuarioDni

--- src/main/java/com/utp/biblioteca/resources/modelo/dao/PrestamoDao.java
+++ src/main/java/com/utp/biblioteca/resources/modelo/dao/PrestamoDao.java
@@ -4,7 +4,6 @@ import com.utp.biblioteca.resources.configuracion.Conexion;
 import com.utp.biblioteca.resources.modelo.Libro;
 import com.utp.biblioteca.resources.modelo.Prestamo;
 import com.utp.biblioteca.resources.modelo.Usuario;
-import com.utp.biblioteca.resources.modelo.dao.sp.StoredProcedureRepository;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -19,18 +18,6 @@ public class PrestamoDao implements CrudDao<Prestamo, Integer> {
     @Override
     public void crear(Prestamo entidad) {
         throw new UnsupportedOperationException("No usen este método. Utilicen 'spRealizarPrestamo' de 'StoredProcedureRepository'.");
-        /*try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement("INSERT INTO Prestamo (usuario_id, libro_id, fecha_prestamo, fecha_limite, fecha_devolucion, devuelto) VALUES (?, ?, ?, ?, ?, ?)")) {
-            ps.setInt(1, entidad.getUsuario().getUsuario_id());
-            ps.setInt(2, entidad.getLibro().getLibro_id());
-            ps.setDate(3, entidad.getFecha_prestamo());
-            ps.setDate(4, entidad.getFecha_limite());
-            ps.setDate(5, entidad.getFecha_devolucion());
-            ps.setBoolean(6, entidad.isDevuelto());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
     }
 
     @Override
@@ -55,6 +42,40 @@ public class PrestamoDao implements CrudDao<Prestamo, Integer> {
                 prestamo.setFecha_devolucion(rs.getDate("fecha_devolucion"));
                 prestamo.setDevuelto(rs.getBoolean("devuelto"));
                 prestamos.add(prestamo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return prestamos;
+    }
+
+    public List<Prestamo> historialUsuarioDni(int dni) {
+        List<Prestamo> prestamos = new ArrayList<>();
+        try (Connection conn = getConnection();
+             PreparedStatement ps = conn.prepareStatement("SELECT * FROM Prestamo WHERE usuario_id = (SELECT usuario_id FROM Usuario WHERE dni = ?)")) {
+            ps.setInt(1, dni);
+
+            try (ResultSet rs = ps.executeQuery()) {
+
+                UsuarioDao usuarioDao = new UsuarioDao();
+                Usuario usuario = usuarioDao.buscarPorDni(dni);
+
+                while (rs.next()) {
+                    Prestamo prestamo = new Prestamo();
+
+                    LibroDao libroDao = new LibroDao();
+                    Libro libro = libroDao.buscarUno(rs.getInt("libro_id"));
+
+                    prestamo.setPrestamo_id(rs.getInt("prestamo_id"));
+                    prestamo.setUsuario(usuario);
+                    prestamo.setLibro(libro);
+                    prestamo.setFecha_prestamo(rs.getDate("fecha_prestamo"));
+                    prestamo.setFecha_limite(rs.getDate("fecha_limite"));
+                    prestamo.setFecha_devolucion(rs.getDate("fecha_devolucion"));
+                    prestamo.setDevuelto(rs.getBoolean("devuelto"));
+
+                    prestamos.add(prestamo);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -94,19 +115,6 @@ public class PrestamoDao implements CrudDao<Prestamo, Integer> {
     @Override
     public void actualizar(Prestamo entidad) {
         throw new UnsupportedOperationException("No usen este método. Utilicen 'spDevolverLibro' de 'StoredProcedureRepository'.");
-        /*try (Connection conn = getConnection();
-             PreparedStatement ps = conn.prepareStatement("UPDATE Prestamo SET usuario_id = ?, libro_id = ?, fecha_prestamo = ?, fecha_limite = ?, fecha_devolucion = ?, devuelto = ? WHERE prestamo_id = ?")) {
-            ps.setInt(1, entidad.getUsuario().getUsuario_id());
-            ps.setInt(2, entidad.getLibro().getLibro_id());
-            ps.setDate(3, entidad.getFecha_prestamo());
-            ps.setDate(4, entidad.getFecha_limite());
-            ps.setDate(5, entidad.getFecha_devolucion());
-            ps.setBoolean(6, entidad.isDevuelto());
-            ps.setInt(7, entidad.getPrestamo_id());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
     }
 
     @Override
