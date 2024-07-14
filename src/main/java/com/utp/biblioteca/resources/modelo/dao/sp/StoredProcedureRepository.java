cc6d3c55ac190af3e3ff8e08ac943a3670781e58
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2024-07-14 00:55:47 -0500

    Reportes mediante sp implementados, se creo un model de frecuencia prestamo para trabajar el reporte correspondiente

--- src/main/java/com/utp/biblioteca/resources/modelo/dao/sp/StoredProcedureRepository.java
+++ src/main/java/com/utp/biblioteca/resources/modelo/dao/sp/StoredProcedureRepository.java
@@ -1,8 +1,17 @@
 package com.utp.biblioteca.resources.modelo.dao.sp;
 
 import com.utp.biblioteca.resources.configuracion.Conexion;
+import com.utp.biblioteca.resources.modelo.Autor;
+import com.utp.biblioteca.resources.modelo.FrecuenciaPrestamo;
+import com.utp.biblioteca.resources.modelo.Libro;
+import com.utp.biblioteca.resources.modelo.Usuario;
+import com.utp.biblioteca.resources.modelo.dao.AutorDao;
+import com.utp.biblioteca.resources.modelo.dao.RolDao;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class StoredProcedureRepository {
 
@@ -37,5 +46,92 @@ public class StoredProcedureRepository {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public List<Libro> spLibrosSinStock() {
+        List<Libro> libros = new ArrayList<>();
+        try (Connection conn = getConnection();
+             CallableStatement cs = conn.prepareCall("{CALL sp_libros_sin_stock()}");
+             ResultSet rs = cs.executeQuery()) {
+            while (rs.next()) {
+                Libro libro = new Libro();
+                libro.setLibro_id(rs.getInt("libro_id"));
+                libro.setIsbn(rs.getString("isbn"));
+                libro.setTitulo(rs.getString("titulo"));
+                libro.setAutor(new AutorDao().buscarUno(rs.getInt("autor_id")));
+                libro.setLink_imagen(rs.getString("link_imagen"));
+                libro.setDescripcion(rs.getString("descripcion"));
+                libro.setStock(rs.getInt("stock"));
+                libros.add(libro);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return libros;
+    }
+
+    // Procedimiento para obtener usuarios atrasados
+    public List<Usuario> spUsuariosAtrasados() {
+        List<Usuario> usuarios = new ArrayList<>();
+        try (Connection conn = getConnection();
+             CallableStatement cs = conn.prepareCall("{CALL sp_usuarios_atrasados()}");
+             ResultSet rs = cs.executeQuery()) {
+            while (rs.next()) {
+                Usuario usuario = new Usuario();
+                usuario.setUsuario_id(rs.getInt("usuario_id"));
+                usuario.setNombres(rs.getString("nombres"));
+                usuario.setApellidos(rs.getString("apellidos"));
+                usuario.setDni(rs.getInt("dni"));
+                usuario.setCorreo(rs.getString("correo"));
+                usuario.setRol(new RolDao().buscarUno(rs.getInt("rol_id")));
+                usuario.setEstado(rs.getBoolean("estado"));
+                usuarios.add(usuario);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
+
+    // Procedimiento para obtener frecuencia de préstamos
+    public List<FrecuenciaPrestamo> spFrecuenciaPrestamos() {
+        List<FrecuenciaPrestamo> frecuenciaPrestamos = new ArrayList<>();
+        try (Connection conn = getConnection();
+             CallableStatement cs = conn.prepareCall("{CALL sp_frecuencia_prestamos()}");
+             ResultSet rs = cs.executeQuery()) {
+            while (rs.next()) {
+                FrecuenciaPrestamo frecuenciaPrestamo = new FrecuenciaPrestamo();
+                frecuenciaPrestamo.setTitulo(rs.getString("titulo"));
+                frecuenciaPrestamo.setFrecuencia(rs.getInt("frecuencia"));
+                frecuenciaPrestamos.add(frecuenciaPrestamo);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return frecuenciaPrestamos;
+    }
+
+    public List<Usuario> spQuienesTienenLibro(int libroId) {
+        List<Usuario> usuarios = new ArrayList<>();
+        try (Connection conn = getConnection();
+             CallableStatement cs = conn.prepareCall("{CALL sp_quienes_tienen_libro(?)}")) {
+            cs.setInt(1, libroId);
+            try (ResultSet rs = cs.executeQuery()) {
+                while (rs.next()) {
+                    Usuario usuario = new Usuario();
+                    usuario.setUsuario_id(rs.getInt("usuario_id"));
+                    usuario.setNombres(rs.getString("nombres"));
+                    usuario.setApellidos(rs.getString("apellidos"));
+                    usuario.setDni(rs.getInt("dni"));
+                    usuario.setCorreo(rs.getString("correo"));
+                    usuario.setRol(new RolDao().buscarUno(rs.getInt("rol_id")));
+                    usuario.setEstado(rs.getBoolean("estado"));
+                    usuarios.add(usuario);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
     }
 }
